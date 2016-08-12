8b7906fec7fab551b78739337f7c42aedf65ad0a
Nikola Majcen <nikola.majcen@gmail.com>
2016-08-12 10:45:50 +0200

    Updated credits and added screenshots

--- Pokedex/ViewControllers/CreditsTableViewController.swift
+++ Pokedex/ViewControllers/CreditsTableViewController.swift
@@ -11,30 +11,45 @@ import UIKit
 class CreditsTableViewController: UITableViewController {
 
     let credits = [
-        ["Any Old Icons",
-            "Designed by TutsPlus and distributed by Flaticon",
-            "http://www.flaticon.com/packs/any-old-icons"],
-        ["Multimedia Collection",
-            "Designed by Gregor Cesnar and distributed by Flaticon",
-            "http://www.flaticon.com/packs/multimedia-collection"],
-        ["Online Shop Icon Collection",
-            "Designed by Becris and distributed by Flaticon",
-            "http://www.flaticon.com/packs/online-shop-icon-collection"]
+        "Artwork": [
+            ["Any Old Icons",
+                "Designed by TutsPlus and distributed by Flaticon",
+                "http://www.flaticon.com/packs/any-old-icons"],
+            ["Multimedia Collection",
+                "Designed by Gregor Cesnar and distributed by Flaticon",
+                "http://www.flaticon.com/packs/multimedia-collection"],
+            ["Online Shop Icon Collection",
+                "Designed by Becris and distributed by Flaticon",
+                "http://www.flaticon.com/packs/online-shop-icon-collection"]
+        ]
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return credits.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionName = Array(credits.keys).sort()[section]
+        return (credits[sectionName]?.count)!
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sectionNames = Array(credits.keys).sort()
+        return sectionNames[section]
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CreditCell", forIndexPath: indexPath)
-        cell.textLabel?.text = credits[indexPath.row][0]
-        cell.detailTextLabel?.text = credits[indexPath.row][1]
-        cell.accessibilityHint = credits[indexPath.row][2]
+        let sectionTitle = Array(credits.keys).sort()[indexPath.section]
+        let creditsValues = credits[sectionTitle]![indexPath.row]
+        
+        cell.textLabel?.text = creditsValues[0]
+        cell.detailTextLabel?.text = creditsValues[1]
+        cell.accessibilityHint = creditsValues[2]
         return cell
     }
 
